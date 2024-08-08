7bf81545962fbdf82f28b0efaee86762a2885c02
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 15:21:35 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let p1 = document.querySelector("#fm1")
+p1.addEventListener("click", function(event){
+p1 = "A strong and prosperous agricultural sector is essential to the well-being of the U.S. economy. America’s farmers and ranchers ensure a reliable food supply, support job growth, and promote economic development. By transforming the food system’s infrastructure and strengthening critical supply chains, USDA will ensure resilience against threats and disturbances and provide local and regional food systems with economic opportunities and security."
+})
